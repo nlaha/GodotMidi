ca25611ec060da0e93dec668aa312898df6b5543
Nathan Laha <nlaha@outlook.com>
2022-12-24 10:49:06 -0800

    Update MidiManager.cs

--- MidiManager.cs
+++ MidiManager.cs
@@ -4,10 +4,20 @@ using Godot;
 [Tool]
 public class MidiManager : Node
 {
+    
+    [Export]
+    public bool autoPlay = true;
+    
+    [Export]
+    public bool autoPlayMusic = true;
+    
     [Signal]
     delegate void NoteEvent(int note, int data, MidiEventNote.NoteType type, int track);
     // Example: EmitSignal(12, 127, MidiEventNote.NoteType.NoteOn, 0);
 
+    private AnimationPlayer _animationPlayer;
+    private AudioStreamPlayer _audioStreamPlayer;
+    
     // ready
     public override void _Ready()
     {
@@ -22,6 +32,43 @@ public class MidiManager : Node
                 
                 // set owner
                 ap.Owner = GetTree().EditedSceneRoot;
+                
+                _animationPlayer = ap;
+            }
+            
+            // if we don't have an audio stream player as a child, add one
+            if (GetNodeOrNull("AudioStreamPlayer") == null)
+            {
+                AudioStreamPlayer asp = new AudioStreamPlayer();
+                    
+                AddChild(asp);
+                
+                // set owner
+                asp.Owner = GetTree().EditedSceneRoot;
+                
+                _audioStreamPlayer = asp;
+            }
+        }
+
+        if (_animationPlayer == null)
+        {
+            // get first animation player child
+            _animationPlayer = GetNodeOrNull<AnimationPlayer>("AnimationPlayer");
+        }
+        
+        if (_audioStreamPlayer == null)
+        {
+            // get first audio stream player child
+            _audioStreamPlayer = GetNodeOrNull<AudioStreamPlayer>("AudioStreamPlayer");
+        }
+        
+        // if auto play is enabled, play the animation
+        if (autoPlay)
+        {
+            _animationPlayer.Play(_animationPlayer.GetAnimationList()[0]);
+            if (autoPlayMusic)
+            {
+                _audioStreamPlayer.Play();
             }
         }
     }
